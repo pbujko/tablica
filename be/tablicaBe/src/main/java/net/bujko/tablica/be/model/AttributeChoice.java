2e5d2a90f1e887af6e92504f1dd0c4e53533e829
Przemo <pbujko@gmail.com>
2011-12-23 09:14:53 +0000

    cd

---
+++ be/tablicaBe/src/main/java/net/bujko/tablica/be/model/AttributeChoice.java
@@ -0,0 +1,14 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package net.bujko.tablica.be.model;
+
+/**
+ *
+ * @author pbujko
+ */
+public class AttributeChoice {
+    String id, label;
+    
+}
